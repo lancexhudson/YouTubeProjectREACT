cfa203fb68e6f0d7f9e1192237d2980a7fdd8331
Lance Hudson <lancexhudson@gmail.com>
2022-09-17 07:48:58 -0400

    ReactDOM setup

--- src/index.js
+++ src/index.js
@@ -1,15 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
-import reducers from './reducers';
+// create a new component that should produce html
+const App = () => {
+    return <div>Hi!</div>;
+}
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
-
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+// renders component to the DOM. second argument determines location
+ReactDOM.render(<App />, document.querySelector('.container')); 
