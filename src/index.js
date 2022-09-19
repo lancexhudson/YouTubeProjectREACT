630833f22efbff2da0e88951a05a23fc8185ed9b
Lance Hudson <lancexhudson@gmail.com>
2022-09-19 12:16:42 -0400

    state and apikey

--- src/index.js
+++ src/index.js
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+const API_KEY = 'AIzaSyBNw2Kjkwg0zQoDUyNDzw6JTztkUF-Wb80';
 
-// create a new component that should produce html
-const App = () => {
-    return <div>Hi!</div>;
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { videos:[] };
+
+        YTSearch({ key: API_KEY, term: 'guitar' }, (videos) => {
+            this.setState({ videos });
+            // this.setState({ videos: videos }); alt for line16
+        });
+    }
+    
+    render() { 
+    return ( <div>
+        <SearchBar />
+    </div>
+    );
 }
+}
+
 
 // renders component to the DOM. second argument determines location
 ReactDOM.render(<App />, document.querySelector('.container')); 
