138d3e31f125b7fea796cfaf1de58ad04bc91582
Lance Hudson <lancexhudson@gmail.com>
2022-09-19 15:39:08 -0400

    video detail / list

--- src/index.js
+++ src/index.js
@@ -2,28 +2,31 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 const API_KEY = 'AIzaSyBNw2Kjkwg0zQoDUyNDzw6JTztkUF-Wb80';
 
 class App extends Component {
     constructor(props) {
         super(props);
 
-        this.state = { videos:[] };
+        this.state = { videos: [] };
 
         YTSearch({ key: API_KEY, term: 'guitar' }, (videos) => {
             this.setState({ videos });
             // this.setState({ videos: videos }); alt for line16
         });
     }
-    
+
     render() { 
     return ( <div>
         <SearchBar />
+        <VideoDetail video={this.state.videos[0]} />
+        <VideoList videos={this.state.videos} />
     </div>
     );
 }
 }
-
 
 // renders component to the DOM. second argument determines location
 ReactDOM.render(<App />, document.querySelector('.container')); 
